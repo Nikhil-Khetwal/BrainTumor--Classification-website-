d469ded051c6adc631285c4a731607ddf9d16a99
Nikhil Khetwal <147499091+Nikhil-Khetwal@users.noreply.github.com>
2024-06-28 16:06:47 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,38 +6,7 @@ import tensorflow as tf
 import streamlit as st
 import time
 import requests
-from bs4 import BeautifulSoup
-import pathlib
-import shutil
 
-# Google Analytics script injection
-def inject_ga():
-    GA_ID = "G-D4GM5C9340"
-    GA_SCRIPT = """
-    <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-D4GM5C9340"></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-
-      gtag('config', 'G-D4GM5C9340');
-    </script>
-    """
-    
-    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
-    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
-    if not soup.find(id=GA_ID): 
-        bck_index = index_path.with_suffix('.bck')
-        if bck_index.exists():
-            shutil.copy(bck_index, index_path)  
-        else:
-            shutil.copy(index_path, bck_index)  
-        html = str(soup)
-        new_html = html.replace('<head>', '<head>\n' + GA_SCRIPT)
-        index_path.write_text(new_html)
-
-inject_ga()
 
 
 
